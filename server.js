ed75aca35c007b9801a87d7845e501310db477ec
oalfonso <oalfonso@yahoo.com>
2017-08-16 23:47:42 +0100

    Unit testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var router = express.Router();              // get an instance of the express Ro
 // Get race data API service
 router.route('/:series/:year/:race') 
 
-    .get(function(req, res,next) {
+    .get(function(req, res) {
 
       var raceData = new Array();  
       var csv = require("fast-csv");
@@ -19,46 +19,53 @@ router.route('/:series/:year/:race')
       var sec = require("./app/seconds.js");
  
       //parsing the CSV file
+     
+      var file = "resources/" + req.params.series + "/" + req.params.year + "/" + req.params.race + ".CSV";
 
-      fs.createReadStream("resources/" + req.params.series + "/" + req.params.year + "/" + req.params.race + ".CSV")
-       .pipe(csv.parse({delimiter: ";", headers: true, trim: true}))
-       .on("data", function(data){
+      fs.stat(file, function(err, stat) {
+        if(err != null) {
+          res.sendStatus(404);
+        }
+        else {
+ 
+          fs.createReadStream(file)
+            .pipe(csv.parse({delimiter: ";", headers: true, trim: true}))
+            .on("data", function(data){
 
-         //We build the array with the lap data
-         raceData.push( { lap: data.LAP_NUMBER,
-                          driver_name: data.DRIVER_NAME,
-                          lap_time: data.LAP_TIME,
-                          segundos: sec.toSeconds(data.LAP_TIME),
-                          class: data.CLASS,
-                          team:  data.TEAM,
-                          manufacturer: data.MANUFACTURER});
+             //We build the array with the lap data
+             raceData.push( { lap: data.LAP_NUMBER,
+                              driver_name: data.DRIVER_NAME,
+                              lap_time: data.LAP_TIME,
+                              segundos: sec.toSeconds(data.LAP_TIME),
+                              class: data.CLASS,
+                              team:  data.TEAM,
+                              manufacturer: data.MANUFACTURER});
 
-       })
-       .on("end", function(){
+             })
+             .on("end", function(){
 
-         //calling the function to calculate the stats
+               //calling the function to calculate the stats
 
-         var st = require("./app/statscalculation.js");
-         var flatData=st.calcStats(raceData);
+              var st = require("./app/statscalculation.js");
+              var flatData=st.calcStats(raceData);
 
-         //sending the output as JSON
+              //sending the output as JSON
        
-         res.contentType('application/json');
-         res.send(JSON.stringify(flatData));
-       });
+              res.contentType('application/json');
+              res.send(JSON.stringify(flatData));
+            });
+        }
+
+      });
 
     });
 
 // all of our routes will be prefixed with /api
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
 console.log('Servidor chutando en ' + port);
+module.exports = app
 
---
+++ test/test.js
@@ -0,0 +1,39 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+chai.use(chaiHttp);
+var expect = chai.expect;
+
+//Our parent block
+describe('Stats api', () => {
+    beforeEach((done) => { });     
+  });
+
+  /*
+  * Test the /GET route
+  */
+  describe('/GET race', () => {
+      it('Returns 404 if the race does not exist', (done) => {
+        chai.request(server)
+            .get('/api/IMSA/2014/LONG_BEACH')
+            .end((err, res) => {
+              res.should.have.status(404);
+              done();
+            });
+      });
+
+      it('Returns 200 if the race exists', (done) => {
+        chai.request(server)
+            .get('/api/WEC/2017/SPA')
+            .end((err, res) => {
+              res.should.have.status(200);
+              done();
+            });
+      });
+  });
